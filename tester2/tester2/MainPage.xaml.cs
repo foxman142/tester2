b699bfa3e0367cafd5864396e39a0864a80f703b
foxman142 <justin.wang38@gmail.com>
2020-10-25 18:23:31 -0500

    Initial commit

---
+++ tester2/tester2/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SkiaSharpFormsDemos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tester2
+{
+    public partial class MainPage : HomeBasePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
